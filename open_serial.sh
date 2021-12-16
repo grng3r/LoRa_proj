4ef53b84cf4f008669ee4c0d3b55ae1ff1291dfa
grng3r <grng3r@protonmail.com>
2021-12-16 18:26:57 +0000

    Import project

---
+++ open_serial.sh
@@ -0,0 +1,4 @@
+#/bin/bash 
+#first copy cmake/*.bin to board
+sudo mbed-tools sterm --baudrate 115200
+
---
+++ sensors/RGB.h
@@ -0,0 +1,12 @@
+#include "mbed.h"
+//RGB SENSOR
+//Only for Information
+#define ENABLE           0x00
+#define TCS34725_CDATAL (0x14) /**< Clear channel data low byte */
+#define TCS34725_CDATAH (0x15) /**< Clear channel data high byte */
+#define TCS34725_RDATAL (0x16) /**< Red channel data low byte */
+#define TCS34725_RDATAH (0x17) /**< Red channel data high byte */
+#define TCS34725_GDATAL (0x18) /**< Green channel data low byte */
+#define TCS34725_GDATAH (0x19) /**< Green channel data high byte */
+#define TCS34725_BDATAL (0x1A) /**< Blue channel data low byte */
+#define TCS34725_BDATAH (0x1B) /**< Blue channel data high byte */
